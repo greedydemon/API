9fac7524146657ad9ca634a3fa8b4b8cfb40dd1e
Tai <Tai@QTHT-PC-TAI>
2022-02-17 16:05:25 +0700

    17/02/2022

---
+++ Models/CategoryAccess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class CategoryAccess
+    {
+        private int _ID; // field
+        public int ID   // property
+        {
+            get { return _ID; }   // get method
+            set { _ID = value; }  // set method
+        }
+
+        private string _Title; // field
+        public string Title   // property
+        {
+            get { return _Title; }   // get method
+            set { _Title = value; }  // set method
+        }
+
+        private string _Department; // field
+        public string Department   // property
+        {
+            get { return _Department; }   // get method
+            set { _Department = value; }  // set method
+        }
+
+    }
+}
--- Utils/DBUtils.cs
+++ Utils/DBUtils.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data.OleDb;
+using API.Models;
 
 namespace API.Utils
 {
@@ -43,16 +44,45 @@ namespace API.Utils
                 return true;
             }
 
-            catch (Exception e)
+            catch (Exception)
             {
-                string text = e.ToString();
                 return false;
             }
         }
+        public List<CategoryAccess> GetAllCategoryAccess()
+        {
+            try
+            {
+                List<CategoryAccess> lstCategoryAccess = new List<CategoryAccess>();
+                myConnection.ConnectionString = myConnectionString;
+                myConnection.Open();
+                string query = "select * from CategoryName";
+                OleDbCommand command = new OleDbCommand(query, myConnection);
+                OleDbDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    CategoryAccess catergoryAccess = new CategoryAccess();
+                    catergoryAccess.ID = Int32.Parse(reader[0].ToString());
+                    catergoryAccess.Title = reader[1].ToString();
+                    catergoryAccess.Department = reader[2].ToString();
+                    lstCategoryAccess.Add(catergoryAccess);
+                }
+                myConnection.Close();
+                return lstCategoryAccess;
+            }
+
+            catch (Exception)
+            {
+                return new List<CategoryAccess>();
+            }
+        }
+
+
 
         public SqlConnection GetDBConnection()
         {
             GetAllConnectInfor();
+            //List<CategoryAccess> _lstcater = GetAllCategoryAccess();
             //string datasource = @"172.20.61.86";
 
             //string database = "TTX_PORTAL_BG";
